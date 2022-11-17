60044857ab3550b0627ebd89fd526650b2214413
rainyraina <rainyraina23@gmail.com>
2022-11-17 16:55:26 +0800

    fd

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -x
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/Seleniumtest/src:/var/www/html php:7.2-apache
+sleep 1
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost:5000 to see your PHP application in action.'
+
