3061bd1cd67730207cb9bca916a9d91663974a50
rainyraina <rainyraina23@gmail.com>
2022-11-17 17:47:17 +0800

    fd

---
+++ src/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home</title>
+    <script>
+      var pattern = /<(.*)>/;
+
+      function validateForm() {
+          let x = document.forms["myForm"]["searchItem"];
+          if (x.value == "") {
+              alert("Invalid Inout");
+              return false;
+          }
+          else {
+              if (pattern.test(x.value) == true) {
+                  x.value = "";
+                  return false;
+              }
+          }
+      }     
+    </script>
+  </head>
+  <body>
+
+    <h2>3X03 Lab Test</h2>
+
+    <form name="myForm" action="result.php" onsubmit="return validateForm()" method="post">
+      <label>Search:</label> 
+      <input type="text" name="searchItem">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+
+  </body>
+</html>
---
+++ src/result.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <title>Results</title>
+    <body>
+        <form method="POST" action="index.php">
+            <?php echo $_POST["searchItem"]?><BR>
+            <button type="submit">Return</button>
+        </form>
+    </body>
+</html>
